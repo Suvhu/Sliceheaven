ff3c87e507dc0fce297c06d327573a4405890de8
Subhashish Swain <122743812+Suvhu@users.noreply.github.com>
2023-11-21 10:16:17 +0530

    Update Adminlogin.jsx

--- src/Screens/Adminlogin.jsx
+++ src/Screens/Adminlogin.jsx
@@ -11,7 +11,7 @@ export default function Adminlogin(props) {
 
 const handleSubmit = async(e)=>{
   e.preventDefault();
-  const response = await fetch("http://localhost:5000/api/authadmin/login",{
+  const response = await fetch("https://slicehesvgh.onrender.com/api/authadmin/login",{
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json',

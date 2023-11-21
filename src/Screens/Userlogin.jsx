1c03d8ce0d6509c487d4cb1f931f4704897b53b0
Subhashish Swain <122743812+Suvhu@users.noreply.github.com>
2023-11-21 10:17:05 +0530

    Update Userlogin.jsx

--- src/Screens/Userlogin.jsx
+++ src/Screens/Userlogin.jsx
@@ -11,7 +11,7 @@ export default function Login(props) {
 
 const handleSubmit = async(e)=>{
   e.preventDefault();
-  const response = await fetch("http://localhost:5000/api/authuser/login",{
+  const response = await fetch("https://slicehesvgh.onrender.com/api/authuser/login",{
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json',

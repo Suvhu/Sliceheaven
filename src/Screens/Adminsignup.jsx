6b88f4b8417b603ff97dd2774198669aab156405
Subhashish Swain <122743812+Suvhu@users.noreply.github.com>
2023-11-21 10:16:39 +0530

    Update Adminsignup.jsx

--- src/Screens/Adminsignup.jsx
+++ src/Screens/Adminsignup.jsx
@@ -19,7 +19,7 @@ export default function Adminsignup(props) {
         e.preventDefault();
         const {name,email,password,cpassword,secretkey} = credentials;
         if(password === cpassword){
-            const response = await fetch("http://localhost:5000/api/authadmin/createadmin",{
+            const response = await fetch("https://slicehesvgh.onrender.com/api/authadmin/createadmin",{
                 method:"POST",
                 headers:{
                     "Content-Type": "application/json",
